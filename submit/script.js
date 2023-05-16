656e27e5e2ac0cb033194de4d91f85d6f49723c5
hjin7221 <hjin7221@gmail.com>
2023-05-16 20:52:58 +0900

    2023-05-16 과제 진행

--- submit/script.js
+++ submit/script.js
@@ -1,8 +1,20 @@
 function adjustLinkWidth() {
   const links = document.querySelectorAll('nav a');
   links.forEach(link => {
-    link.style.width = `${link.parentElement.offsetWidth}px`;
+    link.style.width = 'auto';
   });
+
+  const linkContainerWidth = links[0].parentElement.offsetWidth;
+  const linkCount = links.length;
+  const totalLinkWidth = Array.from(links).reduce((acc, link) => acc + link.offsetWidth, 0);
+  const linkWidth = totalLinkWidth / linkCount;
+
+  if (totalLinkWidth > linkContainerWidth) {
+    links.forEach(link => {
+      link.style.width = `${linkWidth}px`;
+    });
+  }
 }
 
 window.addEventListener('resize', adjustLinkWidth);
+window.addEventListener('load', adjustLinkWidth);
